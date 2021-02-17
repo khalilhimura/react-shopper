31df250e5e23df7c5c7d1052c7dcf76fbecf8880
Khalil Himura <khalil.himura@gmail.com>
2021-02-17 15:53:49 +0800

    #2 Add Routers to React Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Result from "./pages/Result";
 import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/result" component={Result} />
+        <Route path="/:productId" component={Product} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
