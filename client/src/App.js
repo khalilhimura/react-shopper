79c9cc1728a1df45a691ff48b7e368645ddce407
Khalil Himura <khalil.himura@gmail.com>
2021-02-17 17:02:20 +0800

    #5 Intro to React Query

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,13 @@ import Product from "./pages/Product";
 import Result from "./pages/Result";
 import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
@@ -13,6 +17,7 @@ function App() {
         <Route path="/:productId" component={Product} />
       </Switch>
     </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,16 +1,14 @@
 import React from "react";
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { useQuery } from "react-query";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function ProductList() {
-  const [products, setProducts] = React.useState([])
+  const { data: products, isLoading } = useQuery('Products', () => axios('/api/products').then(res => res.data.products))
 
-  React.useEffect(() => {
-    axios.get('api/products')
-      .then(res => res.data.products)
-      .then(products => setProducts(products))
-  }, [])
-
+  if (isLoading) return <LoadingSpinner />
+  
   return products.map(product => (
     <ProductItem key={product.id} product={product} />
   ))
