a871ffb68d4cdec303ea540f560a6a9acf9f1e18
Khalil Himura <khalil.himura@gmail.com>
2021-02-20 00:40:47 +0800

    #8 Handle 404 Errors with Product

--- server/routes.js
+++ server/routes.js
@@ -4,9 +4,25 @@ const products = require("./products.json")
 module.exports = function getRoutes() {
   const router = express.Router();
 
-  router.get('/products', (req, res) => {
-    res.status(200).json({ products })
-  })
+  router.get('/products', getProducts)
+  router.get('/products/:productId', getProduct)
 
   return router;
 };
+
+function getProducts(req, res) {
+  res.status(200).json({ products })
+}
+
+function getProduct(req, res) {
+  const { productId } = req.params
+  const product = products.find(product => product.id === productId)
+  try {
+    if (!product) {
+      throw Error(`No product found for id: ${productId}`)
+    }
+    res.status(200).json({ product })
+  } catch (error) {
+    return res.status(400).json({ statusCode: 404, message: error.message })
+  }
+}
