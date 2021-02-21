648da9197a5a03678f3b416005210677c1425f1f
Khalil Himura <khalil.himura@gmail.com>
2021-02-21 21:33:39 +0800

    #11 Add products to cart

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { loadStripe } from '@stripe/stripe-js'
 import { CartProvider } from "use-shopping-cart";
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ function App() {
         currency="USD"
       >
         <BrowserRouter>
+          <Toaster position="bottom-center" />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/result" component={Result} />
